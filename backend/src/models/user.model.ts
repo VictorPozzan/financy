c6a98ff4a7f7c956e5e750934f978f73eedb92e4
VictorPozzan <victor_pozzan@hotmail.com>
2026-07-31 00:34:55 -0300

    feat: add UserModel for user data representation

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,19 @@
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class UserModel {
+  @Field(() => ID)
+  id!: string;
+
+  @Field(() => String)
+  name!: string;
+
+  @Field(() => String)
+  email!: string;
+
+  @Field(() => Date)
+  createdAt!: Date;
+
+  @Field(() => Date)
+  updatedAt!: Date;
+}
