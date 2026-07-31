137150d5cd7cb8a880c15464c0e212f5e30fb9d4
VictorPozzan <victor_pozzan@hotmail.com>
2026-07-31 11:10:05 -0300

    feat: add Apollo Client setup with authentication link for GraphQL requests

---
+++ frontend/src/lib/graphql/apollo.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client";
+import { SetContextLink } from "@apollo/client/link/context";
+import { useAuthStore } from "@/stores/auth";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:4000";
+
+const httpLink = new HttpLink({ uri: `${backendUrl}/graphql` });
+
+const authLink = new SetContextLink((prevContext) => {
+  const token = useAuthStore.getState().token;
+  return {
+    headers: {
+      ...prevContext.headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export const apolloClient = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
