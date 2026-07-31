2024c4b9fd4e45db2ff6e689fb78434aa401a58e
VictorPozzan <victor_pozzan@hotmail.com>
2026-07-31 11:19:00 -0300

    feat: add GraphQL mutation for updating user profile

---
+++ frontend/src/lib/graphql/mutations/user.ts
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const UPDATE_PROFILE = gql`
+  mutation UpdateProfile($data: UpdateProfileInput!) {
+    updateProfile(data: $data) {
+      id
+      name
+      email
+    }
+  }
+`;
